6bd27f6ab18fa6a4c46781913a3efb50c063b97d
[donovan_torreslan@outlook.com] <donovan_torreslan@outlook.com>
2020-09-28 00:25:08 -0500

    Se crearon las rutas del frontend y se trabaja en la pagina SignIn

---
+++ frontend/src/pages/Admin/SignIn/SignIn.js
@@ -0,0 +1,15 @@
+import React from "react"
+import {Layout, Tabs} from 'antd'
+import { Redirect } from "react-router-dom"
+import './SignIn.scss'
+
+export default function SignIn(){
+ const { Content } = Layout
+ const { TabPanel } = Tabs
+ return(
+  <Layout className="sign-in"
+  <Content className="sign-in_content-logo"
+  
+
+ )
+}
